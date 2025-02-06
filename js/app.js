0ad9a864abc70be0bdedf4f10858a2ae82d18c59
vc3ku <jibrilfatah15@gmail.com>
2025-02-06 17:16:03 +0100

    .

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ const $gameScreenGridCells = document.querySelectorAll(".game-screen-grid__cell"
 const $gameScreenHeaderRestartBtn = document.querySelector(".game-screen-header-restart-btn");
 const $gameScreenScoreScoreNumberX = document.querySelector("#x-player-count");
 const $gameScreenScoreScoreNumberO = document.querySelector("#o-player-count");
+const $gameScreenHeaderTurnIndicator = document.querySelector(".game-screen-header-turn-indicator");
+const $gameScreenEndModal = document.querySelector(".game-screen-end-game-modal")
+const $nextRound = document.querySelector(".game-screen-end-game-modal-buttons__button--secondary")
+
+
+console.log($nextRound);
 
 let currentPlayer = "o";
 let gameBoard = [
@@ -14,6 +20,8 @@ let gameBoard = [
     ["", "", ""]
 ];
 
+
+// Game board icons
 const crossIcon = `<svg width="40" height="40" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" class="game-screen-header-icons__cross">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M27.681 1.63437C26.5094 0.462798 24.6099 0.4628 23.4383 1.63437L16 9.07271L8.56166 1.63437C7.39009 0.462798 5.49059 0.4628 4.31902 1.63437L1.63437 4.31902C0.462799 5.49059 0.462801 7.39009 1.63437 8.56166L9.07271 16L1.63437 23.4383C0.462798 24.6099 0.4628 26.5094 1.63437 27.681L4.31902 30.3656C5.49059 31.5372 7.39009 31.5372 8.56166 30.3656L16 22.9273L23.4383 30.3656C24.6099 31.5372 26.5094 31.5372 27.681 30.3656L30.3656 27.681C31.5372 26.5094 31.5372 24.6099 30.3656 23.4383L22.9273 16L30.3656 8.56166C31.5372 7.39009 31.5372 5.49059 30.3656 4.31902L27.681 1.63437Z"/>
 </svg>`;
@@ -21,6 +29,28 @@ const crossIcon = `<svg width="40" height="40" viewBox="0 0 32 32" fill="none" x
 const ovalIcon = `<svg width="40" height="40" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" class="game-screen-header-icons__oval">
     <path fill-rule="evenodd" clip-rule="evenodd" d="M31.9704 15.8706C31.9704 7.10551 24.8649 0 16.0998 0C7.33476 0 0.229248 7.10551 0.229248 15.8706C0.229248 24.6357 7.33476 31.7412 16.0998 31.7412C24.8649 31.7412 31.9704 24.6357 31.9704 15.8706ZM9.63405 15.8706C9.63405 12.2996 12.5289 9.4048 16.0998 9.4048C19.6708 9.4048 22.5656 12.2996 22.5656 15.8706C22.5656 19.4416 19.6708 22.3364 16.0998 22.3364C12.5289 22.3364 9.63405 19.4416 9.63405 15.8706Z"/>
 </svg>`;
+
+
+// Turn indicator icons
+
+const indicatorCrossIcon = `            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M27.681 1.63437C26.5094 0.462798 24.6099 0.4628 23.4383 1.63437L16 9.07271L8.56166 1.63437C7.39009 0.462798 5.49059 0.4628 4.31902 1.63437L1.63437 4.31902C0.462799 5.49059 0.462801 7.39009 1.63437 8.56166L9.07271 16L1.63437 23.4383C0.462798 24.6099 0.4628 26.5094 1.63437 27.681L4.31902 30.3656C5.49059 31.5372 7.39009 31.5372 8.56166 30.3656L16 22.9273L23.4383 30.3656C24.6099 31.5372 26.5094 31.5372 27.681 30.3656L30.3656 27.681C31.5372 26.5094 31.5372 24.6099 30.3656 23.4383L22.9273 16L30.3656 8.56166C31.5372 7.39009 31.5372 5.49059 30.3656 4.31902L27.681 1.63437Z"
+              />
+            </svg>`
+
+
+const indicatorOvalIcon = `<svg width="16" height="16" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" class="game-screen-header-icons__oval">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M31.9704 15.8706C31.9704 7.10551 24.8649 0 16.0998 0C7.33476 0 0.229248 7.10551 0.229248 15.8706C0.229248 24.6357 7.33476 31.7412 16.0998 31.7412C24.8649 31.7412 31.9704 24.6357 31.9704 15.8706ZM9.63405 15.8706C9.63405 12.2996 12.5289 9.4048 16.0998 9.4048C19.6708 9.4048 22.5656 12.2996 22.5656 15.8706C22.5656 19.4416 19.6708 22.3364 16.0998 22.3364C12.5289 22.3364 9.63405 19.4416 9.63405 15.8706Z"/>
+</svg>`
 
 function checkWin(board) {
     for (let i = 0; i < 3; i++) {
@@ -79,15 +109,18 @@ $gameScreenGridCells.forEach(function ($gameScreenGridCell, index) {
                 $gameScreenGridCell.innerHTML = crossIcon;
                 gameBoard[row][col] = "x";
                 currentPlayer = "o";
+                $gameScreenHeaderTurnIndicator.innerHTML = indicatorOvalIcon + " turn"
             } else {
                 $gameScreenGridCell.innerHTML = ovalIcon;
                 gameBoard[row][col] = "o";
                 currentPlayer = "x";
+                $gameScreenHeaderTurnIndicator.innerHTML = indicatorCrossIcon + " turn"
             }
 
             if (checkWin(gameBoard)) {
                 setTimeout(() => {
                     console.log(`Player ${gameBoard[row][col]} win!`);
+                    $gameScreenEndModal.classList.remove("hidden")
                     if (gameBoard[row][col] === "x") {
                         playerXWinCount++
                         $gameScreenScoreScoreNumberX.innerHTML = playerXWinCount
@@ -102,6 +135,7 @@ $gameScreenGridCells.forEach(function ($gameScreenGridCell, index) {
 });
 
 $gameScreenScoreScoreNumberX.innerHTML = playerXWinCount
+$gameScreenScoreScoreNumberO.innerHTML = playerOWinCount    
 
 
 $gameScreenHeaderRestartBtn.addEventListener("click", function () {
@@ -119,5 +153,25 @@ $gameScreenHeaderRestartBtn.addEventListener("click", function () {
     playerOWinCount = "0"
     $gameScreenScoreScoreNumberX.innerHTML = playerXWinCount
     $gameScreenScoreScoreNumberO.innerHTML = playerOWinCount
+    console.log("Game restarted!");
+});
+
+
+$gameScreenHeaderTurnIndicator.classList.add("game-screen-header-turn-indicator__title");
+
+
+$nextRound.addEventListener("click", function () {
+
+    $gameScreenGridCells.forEach(function ($gameScreenGridCell) {
+        $gameScreenGridCell.innerHTML = "";
+    });
+
+    gameBoard = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ];
+
+    $gameScreenEndModal.classList.add("hidden")
     console.log("Game restarted!");
 });
